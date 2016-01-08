94b7352e042d74a511332450e41ce7813462ffaf
ekstroem <github@ekstroem.com>
2016-01-08 15:15:53 +0100

    Start

--- app.R
+++ app.R
@@ -52,24 +52,17 @@ ui <- fluidPage(
     )
 
 server <- function(input, output) {
-  output$tbl <- renderDataTable(
-    trees,
-    options = list(pageLength = 10),
-    callback = "function(table) {
-    table.on('click.dt', 'tr', function() {
-    $(this).toggleClass('selected').siblings().removeClass('selected');
-    Shiny.onInputChange('rows',
-    table.rows('.selected').indexes().toArray());
-    });
-}"
-  )
 
-  output$dataplot <- renderPlot({
+    ## Do sanity checks
+    if input$n2
+
+  x2 <- sample(, size=, replace=FALSE)
+
+    N <- sapply(1:input$reps, function(i) {input$n1*input$n2 / sum(sample(c(rep(1,input$n1), rep(0, input$n-input$n1)), size=intput$n2))})
+
+  output$plot <- renderPlot({
     par(mar=c(4,4,0,0)+.1, cex=1.4)
-    plot(Volume ~ Height, data=trees, pch=20)
-    if (!is.null(input$rows)) {
-        points(trees$Height[input$rows+1], trees$Volume[input$rows+1], col="red", cex=1.6, pch=20)
-    }
+    hist(N, col="lightblue")
   })
 
 
@@ -77,19 +70,6 @@ server <- function(input, output) {
     paste(c('You selected these rows on the page:', input$rows, "UUU"),
           collapse = ' ')
   })
-
-  # Compute the forumla text in a reactive expression since it is
-  # shared by the output$caption and output$mpgPlot expressions
-    formulaText <- reactive({
-      paste("mpg ~", )
-    })
-
-  # Return the formula text for printing as a caption
-    output$caption <- renderText({
-      formulaText()
-    })
-
-
 
 
 }
